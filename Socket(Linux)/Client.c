4ce7e341b29a04aab7baf10f58fa9750cd44a731
YooSJ <qpxjfdl@naver.com>
2024-07-25 17:41:30 +0900

    C

---
+++ Socket(Linux)/Client.c
@@ -0,0 +1,69 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 8000
+
+int main()
+{
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    char *exit_message = "exit";
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    valread = read(sock, buffer, 1024);
+    buffer[valread] = '\0';
+    printf("%s\n", buffer);
+
+    while (1)
+    {
+        printf("Client: ");
+        fgets(buffer, 1024, stdin);
+        buffer[strcspn(buffer, "\n")] = 0; // Remove newline character
+        send(sock, buffer, strlen(buffer), 0);
+
+        if (strncmp(buffer, exit_message, strlen(exit_message)) == 0)
+        {
+            printf("Exiting...\n");
+            break;
+        }
+
+        printf("Waiting...\n");
+        valread = read(sock, buffer, 1024);
+        buffer[valread] = '\0';
+        printf("Server: %s\n", buffer);
+
+        if (strncmp(buffer, exit_message, strlen(exit_message)) == 0)
+        {
+            printf("Exiting...\n");
+            break;
+        }
+    }
+
+    close(sock);
+    return 0;
+}
---
+++ Socket(Linux)/Server.c
@@ -0,0 +1,78 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 8000
+
+int main()
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *exit_message = "exit";
+    char *connection_message = "Server: Connection established";
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
+        perror("accept failed");
+        exit(EXIT_FAILURE);
+    }
+
+    send(new_socket, connection_message, strlen(connection_message), 0);
+
+    while (1)
+    {
+        printf("Waiting...\n");
+        valread = read(new_socket, buffer, 1024);
+        buffer[valread] = '\0';
+        printf("Client: %s\n", buffer);
+
+        if (strncmp(buffer, exit_message, strlen(exit_message)) == 0)
+        {
+            printf("Exiting...\n");
+            break;
+        }
+
+        printf("Server: ");
+        fgets(buffer, 1024, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+        send(new_socket, buffer, strlen(buffer), 0);
+
+        if (strncmp(buffer, exit_message, strlen(exit_message)) == 0)
+        {
+            printf("Exiting...\n");
+            break;
+        }
+    }
+
+    close(new_socket);
+    close(server_fd);
+    return 0;
+}
