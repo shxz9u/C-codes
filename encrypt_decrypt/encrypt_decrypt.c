03a661b90ab7e703a723c65fb24296e46e82f95e
YooSJ <qpxjfdl@naver.com>
2024-07-26 22:42:11 +0900

    C

---
+++ encrypt_decrypt/encrypt_decrypt.c
@@ -0,0 +1,78 @@
+﻿#include <stdio.h>
+
+#define MAX_LENGTH 256
+
+// 암호화 함수
+void encrypt(char input[], char output[], int key)
+{
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        char ch = input[i];
+
+        if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ((ch - 'a' + key) % 26) + 'a';
+        }
+        else if (ch >= 'A' && ch <= 'Z')
+        {
+            ch = ((ch - 'A' + key) % 26) + 'A';
+        }
+
+        output[i] = ch;
+    }
+}
+
+// 복호화 함수
+void decrypt(char input[], char output[], int key)
+{
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        char ch = input[i];
+
+        if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ((ch - 'a' - key + 26) % 26) + 'a';
+        }
+        else if (ch >= 'A' && ch <= 'Z')
+        {
+            ch = ((ch - 'A' - key + 26) % 26) + 'A';
+        }
+
+        output[i] = ch;
+    }
+}
+
+int main()
+{
+    char input[MAX_LENGTH];
+    char output[MAX_LENGTH] = {0}; // 결과를 저장할 배열, 초기화
+    int key;
+    int choice;
+
+    printf("암호화(1) 또는 복호화(2)를 선택하세요: ");
+    scanf("%d", &choice);
+    getchar(); // 버퍼 비우기
+
+    printf("문자열을 입력하세요: ");
+    fgets(input, MAX_LENGTH, stdin);
+
+    printf("키 값을 입력하세요: ");
+    scanf("%d", &key);
+
+    if (choice == 1)
+    {
+        encrypt(input, output, key);
+        printf("암호화된 문자열: %s\n", output);
+    }
+    else if (choice == 2)
+    {
+        decrypt(input, output, key);
+        printf("복호화된 문자열: %s\n", output);
+    }
+    else
+    {
+        printf("잘못된 선택입니다.\n");
+    }
+
+    return 0;
+}
